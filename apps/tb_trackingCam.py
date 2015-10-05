c99c6d9b112d022d29a9f0be7f9822ddfe438135
Tom Bailey <brimblashman@gmail.com>
2015-10-05 17:45:45 +0100

    editing hotkey loader

--- apps/tb_trackingCam.py
+++ apps/tb_trackingCam.py
@@ -1,8 +1,43 @@
+'''TB Animation Tools is a toolset for animators
+
+*******************************************************************************
+    License and Copyright
+    Copyright 2015-Tom Bailey
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    send issues/ requests to brimblashman@gmail.com
+    visit tb-animator.blogspot.com for "stuff"
+
+*******************************************************************************
+'''
 
 import pymel.core as pm
 import fnmatch
 
 def track(*args):
+    ## a tracking camera that should have a seamless transition between the
+    # default perp view and the tracking camera
+    # usage
+    #
+    # import tb_trackingCam as tb_trackingCam
+    # pick a new tracking target and swap to tracking camera
+    # tb_trackingCam.track("retarget")
+    # return to perspective view
+    # tb_trackingCam.track("persp")
+    # swap to tracking camera
+    # tb_trackingCam.track("tracker")
+    #
     return trackingCamera.DoIt(args)
 
 

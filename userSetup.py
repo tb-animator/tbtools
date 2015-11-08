b65fc6b29f66a3349e0a41f61bbc785bb70621bf
tb-animator <brimblashman@gmail.com>
2015-11-08 20:25:13 +0000

    replacing old hotkey adder in usersetup

--- userSetup.py
+++ userSetup.py
@@ -2,7 +2,9 @@ print "****************************************************************"
 print "\t\ttb-tools module loading\n"
 print "****************************************************************"
 
-import tb_keyCommands as keyCmds
-keyCmds.add_tbtools_commands()
+import tb_keyCommands as tb_hotKeys
+reload(tb_hotKeys)
+tb_hotKeys.hotkey_tool().update_commands()
+tb_hotKeys.hotkey_tool().remove_bad_commands()
 
 # testing something

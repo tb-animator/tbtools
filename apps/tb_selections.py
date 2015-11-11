a4de23dfa9af33f08ba9016e4861e296609d4a4a
Tom Bailey <brimblashman@gmail.com>
2015-11-11 18:31:09 +0000

    quick select tool started

--- apps/tb_selections.py
+++ apps/tb_selections.py
@@ -1,5 +1,6 @@
 __author__ = 'tom.bailey'
 import pymel.core as pm
+import maya.cmds as cmds
 import tb_messages as message
 
 # selects all nodes in a character set
@@ -26,6 +27,49 @@ def select_cheracter_set():
         message.error(position="botRight", prefix="Error", message=msg, fadeStayTime=3.0, fadeOutTime=4.0)
 
 
+# this will find quick selection sets, and if you currently have one object in a set selected
+# it will select the whole set
+
 class quick_selection():
     def __init__(self):
+        self.all_sets = self.get_sets()
+        self.selection = cmds.ls(selection=True)
         pass
+
+    def create_qs_set(self):
+        pass
+
+    def save_qs_to_file(self):
+        pass
+
+    def restore_qs_from_file(self):
+        pass
+
+    def qs_select(self):
+        if self.selection:
+            if self.all_sets:
+                for a_set in self.all_sets:
+                    qs_result = self.check_set_membership(self.selection, a_set)
+                    if qs_result:
+                        cmds.select(a_set, add=True)
+            else:
+                msg = 'no quick selects found for selection'
+                message.error(position="botRight", prefix="Error", message=msg, fadeStayTime=3.0, fadeOutTime=4.0)
+
+
+    @staticmethod
+    def get_sets():
+        all_sets = cmds.ls(sets=True)
+        qs_sets = []
+        for a_set in all_sets:
+            if cmds.sets(a_set, query=True, text=True) == 'gCharacterSet':
+                qs_sets.append(a_set)
+        return qs_sets
+
+    @staticmethod
+    def check_set_membership(selection, sel_set):
+        sel_set_members = cmds.sets(sel_set, query=True)
+        if [ i for i in selection if i in sel_set_members]:
+            return sel_set
+        else:
+            return None

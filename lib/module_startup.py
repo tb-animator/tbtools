cae70706594239dc72cfcb35e98cf38406142c3a
tb-animator <brimblashman@gmail.com>
2019-03-14 00:04:03 -0700

    adding snap tool? This is probably old and needs updating

--- lib/module_startup.py
+++ lib/module_startup.py
@@ -1,14 +1,16 @@
 __author__ = 'Tom'
-
-
+import maya.cmds as cmds
 import maya.utils as mutils
 import tb_optionVars as tbo
 import pymel.core as pm
-
+import maya.api.OpenMaya as Om
 import tb_keyCommands as tb_hotKeys
+import apps.mayaMod.mayaModLoader as mml
+import updater as upd
 reload(tb_hotKeys)
-
-
+import cameraTools.cameraCOI as COI
+reload(COI)
+import maya.mel as mel
 
 
 class initialise():
@@ -17,7 +19,14 @@ class initialise():
 
     def check_for_updates(self):
         upd.updater().check_version()
-
+    '''
+    def dagLoad(self, *args):
+        try:
+            mml.customDagLoader().load()
+            print "re loading dag menu override"
+        except Exception as e:
+            print "BAD CALLBACK", Exception, e
+    '''
     def load_everything(self):
         tb_hotKeys.hotkey_tool().update_commands()
         tb_hotKeys.hotkey_tool().remove_bad_commands()
@@ -27,3 +36,11 @@ class initialise():
         mutils.executeDeferred('import tb_menu as tb_menu;tb_menu.make_ui()')
         mutils.executeDeferred('import updater as upd;upd.updater().check_version()')
         mutils.executeDeferred('import tb_graphEditor as ge;ge.graphEditor().add_graph_editor_callback()')
+
+        # load dag menu edit
+        mutils.executeDeferred('import apps.mayaMod.mayaModLoader as mml;mml.customDagLoader().load()')
+        mutils.executeDeferred('mel.eval(\'scriptJob -conditionTrue \"SomethingSelected\" updateTumble\')')
+        mutils.executeDeferred('mel.eval(\'scriptJob -event \"DragRelease\" updateTumble\')')
+        mutils.executeDeferred('mel.eval(\'scriptJob -event \"ModelPanelSetFocus\" updateTumble\')')
+        mutils.executeDeferred('mel.eval(\'scriptJob -event \"playbackModeChanged\" updateTumble\')')
+        Om.MSceneMessage.addCallback(6, self.dagLoad)
